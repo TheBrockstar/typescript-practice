60e1dae0dcdabe29d1a4bdc210f83c877ca07686
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-03-02 23:21:18 -0700

    Add Endpoints - GET/POST/DELETE People
    - Added some basic endpoints to practice Typescript APIs.

--- lib/app.ts
+++ lib/app.ts
@@ -2,6 +2,16 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 
+let list: any[] = [
+  {
+  id: 0,
+  name: "Brock",
+  age: 28
+  }
+]
+
+let id: number = 1;
+
 class App {
   constructor() {
     this.app = express(); 
@@ -13,7 +23,7 @@ class App {
   
   private config(): void {
     this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({extended: false}));
+    this.app.use(bodyParser.urlencoded({extended: true}));
   } 
   
   private routes(): void {
@@ -22,6 +32,34 @@ class App {
     router.get('/', (_req: Request, res: Response) => {
       res.status(200).send({
         message: "It's a beautiful day!"
+      });
+    });
+
+    router.get('/people', (_req: Request, res: Response) => {
+      res.status(200).send({
+        people: list
+      });
+    });
+
+    router.post('/people', (req: Request, res: Response) => {
+      let name: string = req.body.name; 
+      let age: number = req.body.age;
+
+      list.push({name, age, id});
+
+      id++;
+
+      res.status(200).send({
+        people: list
+      });
+    });
+
+    router.delete('/people/:id', (req: Request, res: Response) => {
+
+      list = list.filter((person) => person.id !== Number(req.params.id));
+
+      res.status(200).send({
+        people: list
       });
     });
   
